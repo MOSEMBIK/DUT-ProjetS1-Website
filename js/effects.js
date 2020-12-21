5fd604e3f62a7058c9e10216dfa474eb50698765
MOSEMBIK <71591390+MOSEMBIK@users.noreply.github.com>
2020-12-21 16:18:11 +0100

    update

--- js/effects.js
+++ js/effects.js
@@ -1,21 +1,18 @@
+var i = 0;
+var txt = 'cybersecurite - pentester';
+var speed = 130;
 
-        var i = 0;
-        var txt = 'Projet PPP Cybersecurite';
-        var speed = 130;
-        
 function typeWriter() {
-        
+
     if (i < txt.length) {
         if (txt.charAt(i) == '/') {
             document.getElementById("transition1").innerHTML += '<br>';
+        } else {
+            document.getElementById("transition1").innerHTML += txt.charAt(i);
         }
-    else {
-        document.getElementById("transition1").innerHTML += txt.charAt(i);
+
     }
-          
-    }
-        
+
     i++;
     setTimeout(typeWriter, speed);
 }
-
